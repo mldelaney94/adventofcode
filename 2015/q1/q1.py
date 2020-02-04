d55bd810693691ca49a2a96ab6ce115e54c631f9
Matthew <mldelaney94@gmail.com>
2020-02-04 20:55:53 +1100

    completed question 1, 2015

---
+++ 2015/q1/q1.py
@@ -0,0 +1,17 @@
+f = open ("inputQ1.txt", "r") # read mode
+
+i = 0
+j = 0
+for line in f:
+    for char in line:
+        if char == '(':
+            i += 1
+        else:
+            i -= 1
+        j += 1
+        if (i == -1):
+            break
+
+f.close()
+
+print (j)
