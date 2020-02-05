a281a32406811f10548e0d71e9572fd5a4702522
Matthew <mldelaney94@gmail.com>
2020-02-05 14:13:39 +1100

    solved q5, 2015

--- 2015/q5/q5.py
+++ 2015/q5/q5.py
@@ -1,11 +1,16 @@
 import re
 f = open ("inputQ5.txt", 'r')
 
-p = re.compile('[aeiou][^ab][^cd][^pq][^xy]*{2,}')
+p = re.compile(".*[aeiou].*[aeiou].*[aeiou].*") # ensure 3 vowels anywhere in the string
+q = re.compile(r".*(\w)\1.*")                   # ensure 2 repeating letters
+t = re.compile("^(?!.*(ab|cd|pq|xy))")          # ensure does not contain ab, cd, pq or xy using negative lookaheads
+x = re.compile(r".*(\w)(\w).*\1\2.*")           # matches 2 pairs of words
+y = re.compile(r".*([a-z]){1}.{1}\1{1}.*")             # matches one letter with a letter between them, like yxy
 
 i = 0
 for line in f:
-    if (p.match(line)):
+    if (y.match(line) and x.match(line)):
+        print(line)
         i += 1
 
 print (i)
