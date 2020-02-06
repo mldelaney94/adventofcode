d6140a44096ed59a884095d4851e3127a1d3e7d7
Matthew <mldelaney94@gmail.com>
2020-02-06 17:02:17 +1100

    added f.close() and solved q6b, 2015

---
+++ 2015/q6/q6b.py
@@ -0,0 +1,34 @@
+f = open ("inputQ6.txt")
+
+import numpy as np
+
+lightArray=np.zeros((1000, 1000), 'int32')
+
+for line in f:
+    line = line.strip()
+    firstSplit = line.split(' ')
+    if firstSplit[0] == 'turn':
+        coordsBottomCorner = firstSplit[2].split(',')
+        coordsTopCorner = firstSplit[4].split(',')
+        xBot = int(coordsBottomCorner[0])
+        yBot = int(coordsBottomCorner[1])
+        xTop = int(coordsTopCorner[0])
+        yTop = int(coordsTopCorner[1])
+        if firstSplit[1] == 'on':
+            lightArray[xBot:xTop+1, yBot:yTop+1] += 1
+        elif firstSplit[1] == 'off':
+            lightArray[xBot:xTop+1, yBot:yTop+1] -= 1
+            lightArray[lightArray < 0] = 0
+    elif firstSplit[0] == 'toggle':
+        coordsBottomCorner = firstSplit[1].split(',')
+        coordsTopCorner = firstSplit[3].split(',')
+        xBot = int(coordsBottomCorner[0])
+        yBot = int(coordsBottomCorner[1])
+        xTop = int(coordsTopCorner[0])
+        yTop = int(coordsTopCorner[1])
+        lightArray[xBot:xTop+1, yBot:yTop+1] += 2
+
+
+print(np.sum(lightArray))
+
+f.close()
