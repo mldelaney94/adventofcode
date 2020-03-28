45f7845fba82c38df93c52c09bd133f5888f69f9
Matthew <mldelaney94@gmail.com>
2020-03-28 11:33:59 +1100

    Completed q16, 2015. Best bit: I did actually misunderstand the question.

--- 2015/q16/q16b.py
+++ 2015/q16/q16b.py
@@ -1,4 +1,8 @@
-""" finds the *REAL* aunt Sue """
+""" finds the *REAL* aunt Sue 
+In particular, what the problem means is that the real Sue has > 7 cats, > 3 trees,
+< 5 goldfish and < 3 pomeranians, so equalling that number is also wrong
+I struggled because I thought it meant that the sue's in the input had greater
+than that many cats and trees"""
 import re
 
 AUNT_SUE = {
@@ -20,16 +24,16 @@ def find_sue():
             break_out = False
             #each susan only has 3 attributes
             curr_sue = re.findall(r"(\d\d*\d*): (\w+): (\d+\d*), (\w+): (\d+\d*), (\w+): (\d+\d*)", line)
-            for index, item in enumerate(curr_sue[0]):
-                if item in ('trees', 'cats'):
-                    if int(curr_sue[0][index+1]) >= AUNT_SUE[item]:
+            for index, attrib in enumerate(curr_sue[0]):
+                if attrib in ('trees', 'cats'):
+                    if int(curr_sue[0][index+1]) <= AUNT_SUE[attrib]:
                         break_out = True
-                elif item in ('goldfish', 'pomeranians'):
-                    if int(curr_sue[0][index+1]) <= AUNT_SUE[item]:
+                elif attrib in ('goldfish', 'pomeranians'):
+                    if int(curr_sue[0][index+1]) >= AUNT_SUE[attrib]:
                         break_out = True
                 else:
                     if index % 2 == 1:
-                        if int(curr_sue[0][index+1]) != AUNT_SUE[item]:
+                        if int(curr_sue[0][index+1]) != AUNT_SUE[attrib]:
                             break_out = True
                 if break_out:
                     break
